38c685ea6ce7aa9f599b031b4e5380081273dd5c
pawelwrzesien <pawel.wrzesien@icloud.com>
2022-08-22 11:13:04 +0200

    Path parameters - pydantic

--- main.py
+++ main.py
@@ -6,3 +6,14 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World!"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id):
+    return {"item_id": item_id}
+
+
+@app.get("/items_with_type/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
